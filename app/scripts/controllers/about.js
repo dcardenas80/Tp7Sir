8625b64b48267e1e893f988f57e9a42ccd56983f
Diego <dym2312@hotmail.com>
2018-03-25 18:21:54 +0200

    yeoman projet

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tp7SirApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the tp7SirApp
+ */
+angular.module('tp7SirApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tp7SirApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the tp7SirApp
+ */
+angular.module('tp7SirApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
